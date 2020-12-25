8e3fc6f4f597b69b171cba2cefb96ddfbaa9a107
沈涛 <taoshen@ucdavis.edu>
2020-12-25 11:44:47 +0800

    tom submit at 1/2

--- src/main/java/com/st/imba/Fortest.java
+++ src/main/java/com/st/imba/Fortest.java
@@ -9,7 +9,8 @@ public class Fortest {
     public void tomfoo1() {
         System.out.println("tom finished at 1/1..");
     }
-    public void benfoo1() {
-        System.out.println("finished at 1/1..");
+
+    public void tomfoo2() {
+        System.out.println("tom finished at 1/2..");
     }
 }
