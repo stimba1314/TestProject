5138b68f9e58839484cf15432c5c5f368ade9ff1
沈涛 <taoshen@ucdavis.edu>
2020-12-25 13:13:37 +0800

    ben

--- src/main/java/com/st/imba/Fortest.java
+++ src/main/java/com/st/imba/Fortest.java
@@ -6,11 +6,15 @@ public class Fortest {
         System.out.println("init of project..");
     }
 
-    public void benfoo1() {
+    public void ben1() {
         System.out.println("Ben finished at 1/1..");
     }
 
-    public void benfoo2() {
+    public void ben2() {
         System.out.println("Ben submit at 1/2");
+    }
+
+    public void ben3() {
+        System.out.println("Ben submit at 1/3");
     }
 }
