3e550bf6f23e6d53a88699e260c0b51b0f1ddeae
沈涛 <taoshen@ucdavis.edu>
2020-12-25 13:02:34 +0800

    minor change

--- src/main/java/com/st/imba/Fortest.java
+++ src/main/java/com/st/imba/Fortest.java
@@ -12,6 +12,7 @@ public class Fortest {
 
     public void tomfoo2() {
         System.out.println("tom finished at 1/2..");
+    }
 
     public void benfoo1() {
         System.out.println("Ben finished at 1/1..");
