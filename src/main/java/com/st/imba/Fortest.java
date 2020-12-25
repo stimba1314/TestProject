85b98f3020d24534a6c865cef5b5800a8a98a7f5
沈涛 <taoshen@ucdavis.edu>
2020-12-25 10:48:31 +0800

    project init

--- src/main/java/com/st/imba/Fortest.java
+++ src/main/java/com/st/imba/Fortest.java
@@ -3,14 +3,6 @@ package com.st.imba;
 public class Fortest {
 
     public void init() {
-        System.out.println("kitty&Ben123");
-    }
-
-
-    public void peter() {
-        System.out.println("Peter&Tomshishabi");
-        System.out.println("Peter&Toaaaajdh");
-        System.out.println("Ben");
-        System.out.println("Tom");
+        System.out.println("init of project..");
     }
 }
